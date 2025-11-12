db15c126d3456a3a58cf3fd154e4f93a993e3c39
epiphanycore <sucharitabiswas.bca2023@nsec.ac.in>
2025-11-12 14:22:33 +0530

    creating new files

---
+++ qrcode/objects.js
@@ -0,0 +1,13 @@
+const student= {
+    name: 'Sucharita Biswas',
+    age: 20,
+    salary: 86000.50,
+    dob: '24-01-2005',
+    hobbies: ['traveling, drawing, playing badminton'],
+    address: {
+        city: "Kolkata",
+        state: "West Bengal",
+        pincode: 700094
+    }
+}
+console.log(student);
